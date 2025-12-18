8d1658e2da7abc06f18b7bbdef56a8fceb9c4ce5
Davinsry <davinipad53@gmail.com>
2025-12-18 12:32:25 +0700

    menambahkan fungsi class interface pada HomeViewModel.kt

--- app/src/main/java/com/example/localrestapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/HomeViewModel.kt
@@ -1,6 +1,12 @@
 package com.example.localrestapi.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.localrestapi.modeldata.DataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed interface StatusUiSiwa{
     data class Success(val listSiswa: List<DataSiswa>) : StatusUiSiwa
@@ -9,3 +15,21 @@ sealed interface StatusUiSiwa{
 }
 
 
+class HomeViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel(){
+    var listSiswa: StatusUiSiwa by mutableStateOf(StatusUiSiwa.Loading)
+        private set
+
+    init{
+        loadSiswa()
+    }
+
+    fun loadSiswa(){
+        viewModelScope.launch {
+            listSiswa = StatusUiSiwa.Loading
+            listSiswa = try{
+                StatusUiSiwa.Success(repositoryDataSiswa.getSiswa())
+            }catch (e: IOException){
+                StatusUiSiwa.Error
+            }
+
+}
