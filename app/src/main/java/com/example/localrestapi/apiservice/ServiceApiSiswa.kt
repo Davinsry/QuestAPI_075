924673cd03f7e01650e054a78edd4a2a695b6f16
Davinsry <davinipad53@gmail.com>
2025-12-18 12:04:14 +0700

    menambahkan fungsi ServiceApiSiswa.kt

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -1,13 +1,16 @@
 package com.example.localrestapi.apiservice
 
 import com.example.localrestapi.modeldata.DataSiswa
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa {
     @GET("bacateman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-
+    @POST("insertTM.php")
+    suspend fun postSiswwa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
 
 }
