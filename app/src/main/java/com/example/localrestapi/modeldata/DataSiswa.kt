6317f872d6a3c23bc2abac9dcd7896a8dea4c151
Davinsry <davinipad53@gmail.com>
2025-12-18 17:03:01 +0700

    perubahan coding yang error

--- app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
@@ -7,7 +7,7 @@ data class DataSiswa(
     val id: Int,
     val nama: String,
     val alamat: String,
-    val telepon: String
+    val telpon: String
 )
 
 data class UIStateSiswa(
@@ -19,14 +19,14 @@ data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telepon: String = "",
+    val telpon: String = "",
 )
 
 fun DetailSiswa.toSiswa(): DataSiswa = DataSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
-    telepon = telepon
+    telpon = telpon
 )
 
 fun DataSiswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
@@ -38,5 +38,5 @@ fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
     id = id,
     nama = nama,
     alamat = alamat,
-    telepon = telepon
+    telpon = telpon
 )
