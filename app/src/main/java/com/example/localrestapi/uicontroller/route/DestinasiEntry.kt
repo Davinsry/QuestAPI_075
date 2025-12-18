e1b5b518e16c7275dadd854faef14111163f43ae
Davinsry <davinipad53@gmail.com>
2025-12-18 17:14:34 +0700

    penambahan fungsi pada DestinasiEntry.kt

---
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,7 @@
+package com.example.localrestapi.uicontroller.route
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
