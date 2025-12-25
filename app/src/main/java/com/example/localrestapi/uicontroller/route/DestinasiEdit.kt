72df490f388dcd81e418dc870601ecc63fdca0d6
Davinsry <davinipad53@gmail.com>
2025-12-25 19:57:44 +0700

    perubahan pada DestinasiDetail dan DestinasiEdit.kt

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiEdit.kt
@@ -1,4 +1,12 @@
 package com.example.localrestapi.uicontroller.route
 
-object DestinasiEdit {
+import com.example.localrestapi.R
+
+object DestinasiEdit : DestinasiNavigasi {
+
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
