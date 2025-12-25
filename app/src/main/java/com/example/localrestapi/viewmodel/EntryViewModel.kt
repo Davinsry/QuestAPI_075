c660717ea16f8c574d468081e0f8537d2ef1ed32
Davinsry <davinipad53@gmail.com>
2025-12-25 19:02:52 +0700

    menambahkan class editviewmodel

--- app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EntryViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.localrestapi.viewmodel
 
-import android.view.View
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -11,30 +10,41 @@ import com.example.localrestapi.modeldata.toDataSiswa
 import com.example.localrestapi.repositori.RepositoryDataSiswa
 import retrofit2.Response
 
+class EntryViewModel(
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
 
-class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
-    ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa):
-            Boolean {
+    // Fungsi untuk memvalidasi input
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
-    fun updateUiState(detailSiswa: DetailSiswa) {
-        uiStateSiswa =
-            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
 
+    // Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
     }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
     suspend fun addSiswa() {
         if (validasiInput()) {
-            val sip: Response<Void> = repositoryDataSiswa
-                .postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
-            if (sip.isSuccessful){
-                println("Sukses tambah data : ${sip.message()}")
-            }else {
+            val sip: Response<Void> =
+                repositoryDataSiswa.postDataSiswa(
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+
+            if (sip.isSuccessful) {
+                println("Sukses Tambah Data : ${sip.message()}")
+            } else {
                 println("Gagal tambah data : ${sip.errorBody()}")
             }
         }
