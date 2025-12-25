7ab217e8d28c9ba0139af865304aaf641bc577ba
Davinsry <davinipad53@gmail.com>
2025-12-25 16:30:45 +0700

    Menambahkan fungsi datasiswaapp detailscreen

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -41,14 +41,24 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
         }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(
+                navArgument(DestinasiDetail.itemIdArg) {
+                    type = NavType.IntType
+                }
+            )
+        ){
+            DetailSiswaScreen(
+                navigateToEditItem = {
+                    navController.navigate("${DestinasiEdit.route}/${it}")
+                },
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
     }
 
-    composable(
-        DestinasiDetail.routeWithArgs,
-        arguments = listOf(
-            navArgument(DestinasiDetail.itemIdArg) {
-                type = NavType.IntType
-            }
-        )
-    )
+
 }
