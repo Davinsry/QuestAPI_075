27339ac8c2133a27a095462fd0c6f08429a4b888
Davinsry <davinipad53@gmail.com>
2025-12-25 16:23:27 +0700

    Menambahkan override get datasiswa

--- app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoryDataSiswa.kt
@@ -33,4 +33,8 @@ class JaringanRepositoryDataSiswa(
 ): RepositoryDataSiswa {
     override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(
+        id: Int
+    ): DataSiswa =
+        serviceApiSiswa.getSatuSiswa(id)
 }
